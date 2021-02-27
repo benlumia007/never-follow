6ff84fdf505ed3b99205bc0800a9137c67a2140d
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-27 10:11:21 -0800

    add link to site title

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Header = ( { siteTitle, siteDescription } ) => (
     <header id="masthead" className="site-header">
         <div className="site-branding">
-            <h1 className="site-title">{ siteTitle }</h1>
+            <h1 className="site-title"><Link to="/">{ siteTitle }</Link></h1>
             <h3 className="site-description">{ siteDescription }</h3>
         </div>
     </header>
