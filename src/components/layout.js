4026396bbd04af856798b3a2503df4482517ca39
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-27 01:26:40 -0800

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,35 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby' useStaticQuery component
+ * 
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import Header from './header';
+
+const Layout = () => {
+    const data = useStaticQuery( graphql`
+    query {
+        site {
+            siteMetadata {
+               title
+               description
+            }
+        }
+    }
+  `)
+
+    return (
+        <>
+            <a className="skip-link screen-reader-text" href="#main">Skip to the content</a>
+            <Header 
+                siteTitle={data.site.siteMetadata.title}
+                siteDescription = {data.site.siteMetadata.description}
+            />
+        </>
+    )
+}
+
+export default Layout;
