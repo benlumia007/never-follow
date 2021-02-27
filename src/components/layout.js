d11cea127009f69a8ffdc78d972ecc337ef1c21d
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-27 09:41:30 -0800

    add footer to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
+import Footer from './footer';
 
 const Layout = () => {
     const data = useStaticQuery( graphql`
@@ -25,9 +26,10 @@ const Layout = () => {
         <>
             <a className="skip-link screen-reader-text" href="#main">Skip to the content</a>
             <Header 
-                siteTitle={data.site.siteMetadata.title}
-                siteDescription = {data.site.siteMetadata.description}
+                siteTitle={ data.site.siteMetadata.title }
+                siteDescription = { data.site.siteMetadata.description }
             />
+            <Footer siteTitle = { data.site.siteMetadata.title } />
         </>
     )
 }
