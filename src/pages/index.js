32bfb59ac710513b2b715e4c043ef9c1b289cd1e
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-02-27 01:28:07 -0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <h1>This is where your shit goes!!</h1>
+        </Layout>
+    )
+}
+
+export default IndexPage;
